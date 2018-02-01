eee87f19db937d9695149c405dc7622259b1e397
Sergey Dmitriev <el.pajaro.sabio@gmail.com>
2018-02-01 16:10:18 +0300

    Refactoring and improvements
    — Changed creation logic of the menu
    — Constants are merged to the main script

--- HandyMenu.sketchplugin/Contents/Sketch/handyMenuDefaults.js
+++
@@ -1,6 +0,0 @@
-const handyMenuPluginID = "com.sergeishere.plugins.handymenu";
-const menuIdentifier = "com.sergeishere.plugins.handymenu.menuWindow";
-const setupMenuIdentifier = "com.sergeishere.plugins.handymenu.setupWindow";
-
-const menuCommandsKey = "plugin_sketch_handymenu_my_commands";
-const menuCommandsCountKey = "plugin_sketch_handymenu_my_commands_count";
--- HandyMenu.sketchplugin/Contents/Sketch/script.js
+++ HandyMenu.sketchplugin/Contents/Sketch/script.js
@@ -1,12 +1,65 @@
 @import "MochaJSDelegate.js";
-@import "handyMenuDefaults.js";
 
-function onRun(context) {
+// Plugin's identifiers
+const HANDY_MENU_PLUGIN_ID = "com.sergeishere.plugins.handymenu";
+const MENU_IDENTIFIER = "com.sergeishere.plugins.handymenu.menuWindow";
+const SETUP_MENU_IDENTIFIER = "com.sergeishere.plugins.handymenu.setupWindow";
 
-    var userDefaults = NSUserDefaults.standardUserDefaults();
-    var threadDictionary = NSThread.mainThread().threadDictionary();
+// User Defaults keys
+const PANEL_COMMANDS_KEY = "plugin_sketch_handymenu_my_commands";
+const NEEDS_RELOAD_KEY = 'handymenu_needs_reload';
+const COMMANDS_COUNT_KEY = "plugin_sketch_handymenu_my_commands_count";
 
-    const itemsCount = userDefaults.integerForKey(menuCommandsCountKey);
+// Handy Meny components sizes
+const COMMAND_ITEM_HEIGHT = 23;
+const MENU_WIDTH = 200;
+
+// System dictionaries
+var userDefaults = NSUserDefaults.standardUserDefaults();
+var threadDictionary = NSThread.mainThread().threadDictionary();
+
+var handyMenuPanel = threadDictionary[MENU_IDENTIFIER];
+
+
+// After loading Sketch
+var onStart = function(context) {
+
+    const itemsCount = userDefaults.integerForKey(COMMANDS_COUNT_KEY);
+    const MENU_HEIGHT = itemsCount * COMMAND_ITEM_HEIGHT + 6;
+
+    // Creating a window
+    handyMenuPanel = NSPanel.alloc().init();
+
+    handyMenuPanel.setFrame_display(NSMakeRect(0, 0, MENU_WIDTH, MENU_HEIGHT), true);
+    handyMenuPanel.setStyleMask(NSWindowStyleMaskTexturedBackground | NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskFullSizeContentView);
+    handyMenuPanel.setBackgroundColor(NSColor.windowBackgroundColor());
+    handyMenuPanel.standardWindowButton(NSWindowCloseButton).setHidden(true);
+    handyMenuPanel.standardWindowButton(NSWindowMiniaturizeButton).setHidden(true);
+    handyMenuPanel.standardWindowButton(NSWindowZoomButton).setHidden(true);
+    handyMenuPanel.setTitlebarAppearsTransparent(true);
+    handyMenuPanel.setLevel(NSPopUpMenuWindowLevel);
+
+    //Add Web View to window
+    var webView = WebView.alloc().initWithFrame(NSMakeRect(0, 0, MENU_WIDTH, MENU_HEIGHT - 3));
+    webView.setAutoresizingMask(NSViewWidthSizable | NSViewHeightSizable);
+    webView.setDrawsBackground(false);
+
+    webView.setMainFrameURL(context.plugin.urlForResourceNamed("handyMenu.html").path());
+    userDefaults.setObject_forKey(true, NEEDS_RELOAD_KEY);
+
+    handyMenuPanel.contentView().addSubview(webView);
+
+    threadDictionary[MENU_IDENTIFIER] = handyMenuPanel;
+
+}
+
+
+// Opening Handy Menu
+var onRun = function(context) {
+
+    COScript.currentCOScript().setShouldKeepAround(true);
+
+    const itemsCount = userDefaults.integerForKey(COMMANDS_COUNT_KEY);
 
     if (itemsCount == 0) {
         context.document.showMessage("Handy Menu is empty.");
@@ -14,64 +67,51 @@ function onRun(context) {
         return;
     }
 
-    if (threadDictionary[menuIdentifier]) {
-        return;
-    }
+    const MENU_HEIGHT = itemsCount * COMMAND_ITEM_HEIGHT + 6;
 
-    const commandItemHeight = 23;
-
-    const windowWidth = 200;
-    const windowHeight = itemsCount * commandItemHeight + 30;
+    log(handyMenuPanel);
 
     var mouseLocation = NSEvent.mouseLocation();
 
-    var xPos = mouseLocation.x;
-    var yPos = mouseLocation.y - windowHeight + 27;
+    var xPos = mouseLocation.x + 1;
+    var yPos = mouseLocation.y - MENU_HEIGHT + 6;
 
-    // Creating a window
-    var webViewWindow = NSPanel.alloc().init();
-    webViewWindow.setFrame_display(NSMakeRect(xPos, yPos, windowWidth, windowHeight), false);
-    webViewWindow.setStyleMask(NSTexturedBackgroundWindowMask | NSTitledWindowMask | NSClosableWindowMask | NSFullSizeContentViewWindowMask);
-    webViewWindow.setBackgroundColor(NSColor.windowBackgroundColor());
-    webViewWindow.standardWindowButton(NSWindowCloseButton).setHidden(true);
-    webViewWindow.standardWindowButton(NSWindowMiniaturizeButton).setHidden(true);
-    webViewWindow.standardWindowButton(NSWindowZoomButton).setHidden(true);
-    webViewWindow.setTitlebarAppearsTransparent(true);
-    webViewWindow.becomeKeyWindow();
-    webViewWindow.setLevel(NSPopUpMenuWindowLevel);
+    if (handyMenuPanel.frame().size.height != MENU_HEIGHT) {
+        handyMenuPanel.setContentSize(NSMakeSize(MENU_WIDTH, MENU_HEIGHT));
+        handyMenuPanel.setFrameOrigin(NSMakePoint(xPos, yPos));
+        handyMenuPanel.contentView().subviews()[0].setFrameSize(NSMakeSize(MENU_WIDTH, MENU_HEIGHT - 3));
+    } else {
+        handyMenuPanel.setFrameOrigin(NSMakePoint(xPos, yPos));
+    }
 
-    threadDictionary[menuIdentifier] = webViewWindow;
+    if (userDefaults.boolForKey(NEEDS_RELOAD_KEY)) {
+        log('reloaded');
+        handyMenuPanel.contentView().subviews()[0].reload(nil);
+        userDefaults.setObject_forKey(false, NEEDS_RELOAD_KEY);
+    }
 
-    COScript.currentCOScript().setShouldKeepAround(true);
+    handyMenuPanel.makeKeyAndOrderFront(nil);
 
-    //Add Web View to window
-    var webView = WebView.alloc().initWithFrame(NSMakeRect(0, 0, windowWidth, windowHeight - 26));
-    webView.setAutoresizingMask(NSViewWidthSizable | NSViewHeightSizable);
-    webView.setDrawsBackground(false);
-    var windowObject = webView.windowScriptObject();
+    var constWebView = handyMenuPanel.contentView().subviews()[0];
+    var windowObject = constWebView.windowScriptObject();
+    var myCommandsString = userDefaults.stringForKey(PANEL_COMMANDS_KEY);
+
     var delegate = new MochaJSDelegate({
 
         "webView:didFinishLoadForFrame:": (function(webView, webFrame) {
 
-            var myCommandsString = userDefaults.stringForKey(menuCommandsKey);
             windowObject.callWebScriptMethod_withArguments('updateCommandsList', [myCommandsString]);
 
         }),
 
-        //To get commands from the webView we observe the location hash: if it changes, we do something
         "webView:didChangeLocationWithinPageForFrame:": (function(webView, webFrame) {
 
             var locationHash = windowObject.evaluateWebScript("window.location.hash");
-            //The hash object exposes commands and parameters
-            //In example, if you send updateHash('add','artboardName','Mark')
-            //You’ll be able to use hash.artboardName to return 'Mark'
+
             var hash = parseHash(locationHash);
             log(hash);
-            //We parse the location hash and check for the command we are sending from the UI
-            //If the command exist we run the following code
+
             if (hash.hasOwnProperty('executeCommand')) {
-                //If you are sending arguments from the UI
-                //You can simply grab them from the hash object
 
                 commandID = hash.commandID;
                 pluginID = hash.pluginID;
@@ -84,11 +124,12 @@ function onRun(context) {
                     var commands = plugins[key].commands();
                     for (command in commands) {
                         if (key == pluginID && commands[command].metadata().identifier == commandID) {
-                            webViewWindow.close();
-                            threadDictionary.removeObjectForKey(menuIdentifier);
-                            commands[command].run_manager(context, pluginManager);
+
+                            handyMenuPanel.orderOut(nil);
+                            commands[command].run_manager(updateContext().doc, pluginManager);
+
                             COScript.currentCOScript().setShouldKeepAround(false);
-                            return
+                            return;
                         }
                     }
                 }
@@ -97,50 +138,26 @@ function onRun(context) {
         })
     });
 
-    webView.setFrameLoadDelegate_(delegate.getClassInstance());
-    webView.setMainFrameURL_(context.plugin.urlForResourceNamed("handyMenu.html").path());
-    webViewWindow.contentView().addSubview(webView);
-
-    webViewWindow.makeKeyAndOrderFront(nil);
+    constWebView.setFrameLoadDelegate_(delegate.getClassInstance());
 
     // Define the close window behaviour on the standard red traffic light button
-    var closeButton = webViewWindow.standardWindowButton(NSWindowCloseButton);
+    var closeButton = handyMenuPanel.standardWindowButton(NSWindowCloseButton);
     closeButton.setCOSJSTargetFunction(function(sender) {
         COScript.currentCOScript().setShouldKeepAround(false);
-        threadDictionary.removeObjectForKey(menuIdentifier);
-        webViewWindow.close();
+        handyMenuPanel.orderOut(nil);
     });
     closeButton.setAction("callAction:");
 };
 
-function onSetup(context) {
+// Handy Menu settings window
+var onSetup = function(context) {
 
     var userDefaults = NSUserDefaults.standardUserDefaults();
     var threadDictionary = NSThread.mainThread().threadDictionary();
 
-    if (threadDictionary[setupMenuIdentifier]) {
+    if (threadDictionary[SETUP_MENU_IDENTIFIER]) {
         return;
     }
-
-    COScript.currentCOScript().setShouldKeepAround(true);
-
-    // Configuring a window
-    var windowWidth = 640;
-    var windowHeight = 640;
-
-    var webViewWindow = NSPanel.alloc().init();
-
-    webViewWindow.setFrame_display(NSMakeRect(0, 0, windowWidth, windowHeight), false);
-    webViewWindow.setStyleMask(NSTitledWindowMask | NSClosableWindowMask);
-    webViewWindow.setBackgroundColor(NSColor.colorWithRed_green_blue_alpha(0.13, 0.07, 0.33, 1.0));
-    webViewWindow.standardWindowButton(NSWindowMiniaturizeButton).setHidden(true);
-    webViewWindow.standardWindowButton(NSWindowZoomButton).setHidden(true);
-    webViewWindow.setTitlebarAppearsTransparent(true);
-    webViewWindow.becomeKeyWindow();
-    webViewWindow.setLevel(NSFloatingWindowLevel);
-    webViewWindow.isMovable = true;
-
-    threadDictionary[setupMenuIdentifier] = webViewWindow;
 
     // Getting commands list
     var pluginManager = AppController.sharedInstance().pluginManager();
@@ -149,7 +166,7 @@ function onSetup(context) {
     var allCommands = [];
 
     for (key in plugins) {
-        if (key != handyMenuPluginID) {
+        if (key != HANDY_MENU_PLUGIN_ID) {
             var commands = plugins[key].commands();
             var record = {
                 'pluginName': plugins[key].metadata().name + '',
@@ -172,54 +189,67 @@ function onSetup(context) {
             }
 
             allCommands.push(record);
-            
+
         }
     }
 
+    // Configuring a window
+    var windowWidth = 640;
+    var MENU_HEIGHT = 640;
+
+    var webViewWindow = NSPanel.alloc().init();
+
+    webViewWindow.setFrame_display(NSMakeRect(0, 0, windowWidth, MENU_HEIGHT), false);
+    webViewWindow.setStyleMask(NSTitledWindowMask | NSClosableWindowMask);
+    webViewWindow.setBackgroundColor(NSColor.colorWithRed_green_blue_alpha(0.13, 0.07, 0.33, 1.0));
+    webViewWindow.standardWindowButton(NSWindowMiniaturizeButton).setHidden(true);
+    webViewWindow.standardWindowButton(NSWindowZoomButton).setHidden(true);
+    webViewWindow.setTitlebarAppearsTransparent(true);
+    webViewWindow.setLevel(NSFloatingWindowLevel);
+
+    threadDictionary[SETUP_MENU_IDENTIFIER] = webViewWindow;
+
     const allCommandsString = JSON.stringify(allCommands);
 
+    COScript.currentCOScript().setShouldKeepAround(true);
     //Add Web View to window
-    var webView = WebView.alloc().initWithFrame(NSMakeRect(0, 0, windowWidth, windowHeight - 24));
+    var webView = WebView.alloc().initWithFrame(NSMakeRect(0, 0, windowWidth, MENU_HEIGHT - 24));
     webView.setAutoresizingMask(NSViewWidthSizable | NSViewHeightSizable);
     var windowObject = webView.windowScriptObject();
     var delegate = new MochaJSDelegate({
 
         "webView:didFinishLoadForFrame:": (function(webView, webFrame) {
 
-            var myCommandsString = userDefaults.stringForKey(menuCommandsKey);
+            var myCommandsString = userDefaults.stringForKey(PANEL_COMMANDS_KEY);
 
             windowObject.callWebScriptMethod_withArguments("loadMyCommandsList", [myCommandsString]);
             windowObject.callWebScriptMethod_withArguments("loadAllCommandsList", [allCommandsString]);
 
         }),
 
-        //To get commands from the webView we observe the location hash: if it changes, we do something
         "webView:didChangeLocationWithinPageForFrame:": (function(webView, webFrame) {
 
             var locationHash = windowObject.evaluateWebScript("window.location.hash");
-            //The hash object exposes commands and parameters
-            //In example, if you send updateHash('add','artboardName','Mark')
-            //You’ll be able to use hash.artboardName to return 'Mark'
             var hash = parseHash(locationHash);
             log(hash);
-            //We parse the location hash and check for the command we are sending from the UI
-            //If the command exist we run the following code
+
             if (hash.hasOwnProperty('saveCommandsList')) {
-                //If you are sending arguments from the UI
-                //You can simply grab them from the hash object
                 var commandsString = hash.commands;
                 var commandsCount = hash.commandsCount;
 
-                userDefaults.setObject_forKey(commandsString, menuCommandsKey);
-                userDefaults.setObject_forKey(commandsCount, menuCommandsCountKey);
+                userDefaults.setObject_forKey(commandsString, PANEL_COMMANDS_KEY);
+                userDefaults.setObject_forKey(commandsCount, COMMANDS_COUNT_KEY);
                 userDefaults.synchronize();
 
-                threadDictionary.removeObjectForKey(setupMenuIdentifier);
+                threadDictionary.removeObjectForKey(SETUP_MENU_IDENTIFIER);
                 webViewWindow.close();
+
+                userDefaults.setObject_forKey(true, NEEDS_RELOAD_KEY);
+
                 COScript.currentCOScript().setShouldKeepAround(false);
             } else if (hash.hasOwnProperty('closeWindow')) {
                 webViewWindow.close();
-                threadDictionary.removeObjectForKey(setupMenuIdentifier);
+                threadDictionary.removeObjectForKey(SETUP_MENU_IDENTIFIER);
                 COScript.currentCOScript().setShouldKeepAround(false);
             }
         })
@@ -231,17 +261,21 @@ function onSetup(context) {
     webViewWindow.center();
     webViewWindow.makeKeyAndOrderFront(nil);
 
-    // Define the close window behaviour on the standard red traffic light button
     var closeButton = webViewWindow.standardWindowButton(NSWindowCloseButton);
     closeButton.setCOSJSTargetFunction(function(sender) {
         COScript.currentCOScript().setShouldKeepAround(false);
-        threadDictionary.removeObjectForKey(setupMenuIdentifier);
+        threadDictionary.removeObjectForKey(SETUP_MENU_IDENTIFIER);
         webViewWindow.close();
     });
     closeButton.setAction("callAction:");
 };
 
-//Utility functions
+
+
+// Utility functions
+
+
+// Getting actual context
 function updateContext() {
     var doc = NSDocumentController.sharedDocumentController().currentDocument();
 
@@ -250,14 +284,20 @@ function updateContext() {
     }
 }
 
+
+// Getting Hex Color from NSColor
+
 function getHexColor(fromNSColor) {
+
     var color = MSColor.colorWithNSColor(fromNSColor));
-var sR = (Math.round(color.red() * 255)).toString(16);
-var sG = (Math.round(color.green() * 255)).toString(16);
-var sB = (Math.round(color.blue() * 255)).toString(16);
-return "#" + sR + sG + sB;
+    var sR = (Math.round(color.red() * 255)).toString(16);
+    var sG = (Math.round(color.green() * 255)).toString(16);
+    var sB = (Math.round(color.blue() * 255)).toString(16);
+    return "#" + sR + sG + sB;
 }
 
+
+// Parsing WebView's hash
 function parseHash(aURL) {
     aURL = aURL;
     var vars = {};
