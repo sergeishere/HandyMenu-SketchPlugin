5c044c99994609fd5d493b383cf4face3e079813
Sergey Dmitriev <el.pajaro.sabio@gmail.com>
2018-03-19 01:27:57 +0300

    Updated Readme.md

---
+++ HandyMenuFramework/HandyMenuFramework/HMMenuManager.m
@@ -0,0 +1,13 @@
+//
+//  HMMenuManager.m
+//  HandyMenuFramework
+//
+//  Created by Sergey Dmitriev on 18/03/2018.
+//  Copyright © 2018 Sergey Dmitriev. All rights reserved.
+//
+
+#import "HMMenuManager.h"
+
+@implementation HMMenuManager
+
+@end
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![Cover](images/cover_Image.png)
+![Cover](Images/cover_Image.png)
 
 # Handy Menu Sketch Plugin
 ## Instalation
