9120ab8f14ffbf6a8600bc6aeea1cd8e703657cb
Sergey Dmitriev <el.pajaro.sabio@gmail.com>
2020-06-25 21:49:34 +0300

    `.gitignore` has been added. Project's folders structure has been prepared to future changes.

---
+++ Contents/Resources/HandyMenuFramework.framework/Versions/A/Headers/HandyMenuFramework.h
@@ -0,0 +1,21 @@
+//
+//  HandyMenuFramework.h
+//  HandyMenuFramework
+//
+//  Created by Sergey Dmitriev on 16/07/2018.
+//  Copyright © 2018 Sergey Dmitriev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "HMCommandScheme.h"
+#import "SketchAppBridge.h"
+
+//! Project version number for HandyMenuFramework.
+FOUNDATION_EXPORT double HandyMenuFrameworkVersionNumber;
+
+//! Project version string for HandyMenuFramework.
+FOUNDATION_EXPORT const unsigned char HandyMenuFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HandyMenuFramework/PublicHeader.h>
+
+
